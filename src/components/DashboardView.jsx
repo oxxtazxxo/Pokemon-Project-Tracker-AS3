4f671c2bdd659339fff19c7c23c507b9e60ea4a0
Alyssa Scott <lilzkin4@gmail.com>
2026-06-24 14:53:39 -0500

    update dashboard view to use summary panel and task list components

--- src/components/DashboardView.jsx
+++ src/components/DashboardView.jsx
@@ -1,3 +1,6 @@
+import SummaryPanel from "./SummaryPanel";
+import TaskList from "./TaskList";
+
 function DashboardView({ tasks})
 {
     return(
@@ -5,17 +8,22 @@ function DashboardView({ tasks})
             <h2>Project Dashboard</h2>
             <p>Track your Pokemon Info Finder development and tasks here ^^.</p>
 
+            {/* summary stats section */}
+            <SummaryPanel tasks={tasks} />
+
+            {/* task list section */}
+            <TaskList tasks={tasks}/>
             {/* Dashboard summary */}
             <h3>Total Tasks: {tasks.length}</h3>
 
-            {/* Temporary task preview */}
-            <ul>
+            {/* Temporary task preview. Used for testing purposes! */}
+            {/*<ul>
                 {tasks.map((task) => (
                 <li key={task.id}>
                     <strong>{task.title}</strong> - {task.category} - {task.status}
                 </li>
                 ))}
-            </ul>
+            </ul>*/}
         </section>
     );
 }
