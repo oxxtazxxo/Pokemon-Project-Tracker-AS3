6d2a7d745de40365552727e700e7765063ef3994
Alyssa Scott <lilzkin4@gmail.com>
2026-06-24 12:25:25 -0500

    display task data in dashboard view using props and map

--- src/components/DashboardView.jsx
+++ src/components/DashboardView.jsx
@@ -1,9 +1,21 @@
-function DashboardView()
+function DashboardView({ tasks})
 {
     return(
         <section className="dashboard-view">
             <h2>Project Dashboard</h2>
-            <p>Track your Pokemon Info Finder development and tasks here.</p>
+            <p>Track your Pokemon Info Finder development and tasks here ^^.</p>
+
+            {/* Dashboard summary */}
+            <h3>Total Tasks: {tasks.length}</h3>
+
+            {/* Temporary task preview */}
+            <ul>
+                {tasks.map((task) => (
+                <li key={task.id}>
+                    <strong>{task.title}</strong> - {task.category} - {task.status}
+                </li>
+                ))}
+            </ul>
         </section>
     );
 }
