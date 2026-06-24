f64624a36f0107b39e41de38c82ebdd2f4f6edbc
Alyssa Scott <lilzkin4@gmail.com>
2026-06-24 11:25:08 -0500

    since im still learning, adding in comments. also forgot to add the import {use state}

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+// useState = being able to use/create "state" in a component
+import { useState } from "react";
+// import the .css file for app styling
+import "./App.css";
+
+// import child components :3
 import Header from "./components/Header";
 import ViewSwitcher from "./components/ViewSwitcher";
 import DashboardView from "./components/DashboardView";
@@ -5,18 +11,23 @@ import AboutView from "./components/AboutView";
 
 function App()
 {
+  // view stores which section of the app is visible
+  // "dashboard" will show the main stack tracker page first
   const [view, setView] = useState("dashboard");
 
   return
   (
     <div classname="app">
+      /* Header component shows the app title and intro blahblah */
       <Header />
-
+      /* Viewswitcher gets the current view and function.
+      Also lets me click buttons like dashboard and about */
       <viewSwitcher view={view} setView={setView} />
 
+      /* if view === "dashboard", then shows dashboardview, if not, show aboutview */
       {view === "dashboard" ? <DashboardView /> : <AboutView />}
     </div>
   );
 }
-
+// exporting!
 export default App;
